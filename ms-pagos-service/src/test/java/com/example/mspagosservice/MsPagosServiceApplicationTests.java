ae494654758457520b676eac343ceda839af10d2
Hector <pacompiahectorrobert@gmail.com>
2025-04-09 23:57:27 -0500

    pagos

---
+++ ms-pagos-service/src/test/java/com/example/mspagosservice/MsPagosServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.mspagosservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsPagosServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
